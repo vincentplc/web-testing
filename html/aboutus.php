e61b6fbc1e1489f5a4d4bd6be05054d69543e4e5
Vincent Leung (W10PLC55) <vincentleung@plc.com.hk>
2017-03-28 14:18:53 +0800

    Marge all pages together

--- html/aboutus.php
+++ html/aboutus.php
@@ -5,6 +5,5 @@
 <h1>Who am I?</h1>
 <hr>
 I am who I am.
-<a href="contactus.php">Contact us!</a>
 </body>
 </html>
--- html/index.php
+++ html/index.php
@@ -7,6 +7,7 @@ Front Page
 <ol>
 <li><a href="aboutus.php">About Us</a></li>
 <li><a href="product.php">Our Product</a></li>
+<li><a href="contactus.php">Contact us!</a></li>
 </ol>
 </body>
 </html>
